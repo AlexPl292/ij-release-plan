3acc80ac6b8d916c8d8a46374e151ab426916e3d
Alex Plate <aleksei.plate@jetbrains.com>
2022-11-24 21:00:30 +0200

    Make better rendering

--- src/main/kotlin/com/github/alexpl292/ijreleaseplan/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexpl292/ijreleaseplan/services/MyProjectService.kt
@@ -1,9 +1,11 @@
 package com.github.alexpl292.ijreleaseplan.services
 
 import com.github.alexpl292.ijreleaseplan.MyBundle
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep
+import com.intellij.openapi.util.NlsContexts.PopupTitle
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
@@ -13,6 +15,7 @@ import java.awt.Point
 import java.awt.event.MouseEvent
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
+import javax.swing.Icon
 
 class MyProjectService(project: Project) {
 
@@ -57,16 +60,27 @@ class VimStatusBar : StatusBarWidget, StatusBarWidget.TextPresentation {
     }
 
     override fun getClickConsumer() = Consumer<MouseEvent> {
-        val sortedDates = dates.sortedBy { it.first }
         val now = LocalDate.now()
-        val items = sortedDates.take(5).map { "${it.second}: ${it.first} (in ${ChronoUnit.DAYS.between(now, it.first)} days)" }
-        val popup = JBPopupFactory.getInstance().createListPopup(BaseListPopupStep("Nearest: ${items.first()}", items.drop(1)))
+        val structure = info.take(10).filter { it.date.isAfter(now) }.sortedBy { it.date }.groupBy { it.project }.mapValues { it.value.groupBy { it.version } }
+        val items = buildList {
+            structure.forEach { project, projectItems ->
+                this.add(project)
+                projectItems.forEach { version, items ->
+                    this.add(version)
+                    items.forEach { item ->
+                        this.add(" - ${item.releaseType}: ${item.date} (in ${ChronoUnit.DAYS.between(now, item.date)} days)")
+                    }
+                }
+            }
+        }
+        val popup = JBPopupFactory.getInstance().createListPopup(MyPopup(text, items))
         popup.show(RelativePoint(it.component, Point(0, -popup.content.preferredSize.height)))
     }
 
+    @PopupTitle
     override fun getText(): String {
         val now = LocalDate.now()
-        return dates.sortedBy { it.first }.first().let { "in ${ChronoUnit.DAYS.between(now, it.first)} days" }
+        return info.filter { it.date.isAfter(now) }.minBy { it.date }.let { "${it.shortName} ${it.releaseType} in ${ChronoUnit.DAYS.between(now, it.date)} days" }
     }
 
     override fun getAlignment(): Float = 0.0F
@@ -74,12 +88,33 @@ class VimStatusBar : StatusBarWidget, StatusBarWidget.TextPresentation {
     override fun getPresentation() = this
 }
 
-val dates = listOf(
-        LocalDate.of(2022, 11, 29) to "2022.3 Release",
-        LocalDate.of(2022, 12, 6) to "2022.3.1 Preview",
-        LocalDate.of(2022, 12, 13) to "2022.3.1 RC",
-        LocalDate.of(2022, 12, 20) to "2022.3.1 Release",
-        LocalDate.of(2023, 1, 10) to "2022.3.2 Preview",
-        LocalDate.of(2023, 1, 17) to "2022.3.2 RC",
-        LocalDate.of(2023, 1, 24) to "2022.3.2 Release",
+private class MyPopup(header: String, items: List<String>) : BaseListPopupStep<String>(header, items) {
+    override fun isSelectable(value: String?) = false
+
+    override fun getIconFor(value: String?): Icon? {
+        return if (value == "IntelliJ IDEA") {
+            AllIcons.Debugger.Db_muted_breakpoint
+        } else {
+            null
+        }
+    }
+}
+
+data class ReleaseItem(
+        val project: String,
+        val shortName: String,
+        val releaseType: String,
+        val date: LocalDate,
+        val version: String,
+)
+
+val info = listOf(
+        ReleaseItem("IntelliJ IDEA", "IJ", "Beta", LocalDate.of(2022, 11, 1), "2022.3"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "Release", LocalDate.of(2022, 11, 29), "2022.3"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "Preview", LocalDate.of(2022, 12, 6), "2022.3.1"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "RC", LocalDate.of(2022, 12, 13), "2022.3.1"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "Release", LocalDate.of(2022, 12, 20), "2022.3.1"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "Preview", LocalDate.of(2023, 1, 10), "2022.3.2"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "RC", LocalDate.of(2023, 1, 17), "2022.3.2"),
+        ReleaseItem("IntelliJ IDEA", "IJ", "Release", LocalDate.of(2023, 1, 24), "2022.3.2"),
 )
